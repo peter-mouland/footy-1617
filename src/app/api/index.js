f5b5dec361551e321d0c7357799f72a2d21875be
Peter Mouland <peter.mouland@gmail.com>
2016-08-17 09:36:52 +0100

    refactor client-side api/index

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,31 +1,4 @@
-import fetch from 'isomorphic-fetch';
-import debug from 'debug';
-
-import { checkStatus } from './utils';
-import { localUrl } from '../utils';
-
-const log = debug('footy:api/index');
-
-const jsonOpts = (method, data) => ({
-  method,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(data)
-});
-
-const fetchUrl = (url, opts) => {
-  return fetch(`${localUrl}/${url}`, opts)
-    .then(checkStatus)
-    .then((res) => res.json())
-    .then((json) => json)
-    .catch((error) => {
-      log('request failed', error);
-    });
-};
-const getJSON = (url) => fetchUrl(url, jsonOpts('GET'));
-const postJSON = (url, data) => fetchUrl(url, jsonOpts('POST', data));
+import { getJSON, postJSON } from './utils';
 
 export default {
   fetchPlayers() {
--- src/app/api/utils.js
+++ src/app/api/utils.js
@@ -1,3 +1,9 @@
+import fetch from 'isomorphic-fetch';
+import debug from 'debug';
+
+import { localUrl } from '../utils';
+
+const log = debug('footy:api/index');
 
 export function checkStatus(response) {
   if (response.status < 200 || response.status >= 300) {
@@ -7,3 +13,24 @@ export function checkStatus(response) {
   }
   return response;
 }
+
+const jsonOpts = (method, data) => ({
+  method,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(data)
+});
+
+const fetchUrl = (url, opts) => {
+  return fetch(`${localUrl}/${url}`, opts)
+    .then(checkStatus)
+    .then((res) => res.json())
+    .then((json) => json)
+    .catch((error) => {
+      log('request failed', error);
+    });
+};
+export const getJSON = (url) => fetchUrl(url, jsonOpts('GET'));
+export const postJSON = (url, data) => fetchUrl(url, jsonOpts('POST', data));
